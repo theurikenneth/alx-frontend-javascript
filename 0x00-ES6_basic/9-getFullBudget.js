c72e9eba3380bd9587f5b72f6590467407cae385
theurikenneth <theurikenneth@gmail.com>
2021-11-23 08:43:33 +0000

    ES6 Basics

---
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -0,0 +1,13 @@
+// use ES6 method properties in the fullBudget object
+import getBudgetObject from './7-getBudgetObject.js';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars: (income) => `$${income}`,
+    getIncomeInEuros: (income) => `${income} euros`,
+  };
+
+  return fullBudget;
+}
