389c021725f089cd298d3754d47c168815fef8f0
theurikenneth <theurikenneth@gmail.com>
2021-11-23 09:40:07 +0000

    ES6 Basics

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,10 +1,4 @@
+// function named createReportObject whose parameter, employeesList
 export default function createReportObject(employeesList) {
-  return {
-    allEmployees: {
-      ...employeesList,
-    },
-    getNumberofDepartments(employeesList) {
-      return Object.keys(employeesList).length;
-    },
-  };
+  return { allEmployees: { ...employeesList } };
 }
