577d52b771d319ed8b5c1cc93cc186e2b4709693
theurikenneth <theurikenneth@gmail.com>
2021-11-23 07:30:21 +0000

    ES6 Basics

---
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -0,0 +1,5 @@
+/* defines default parameter values
+for the function parameters */
+export default function getSumOfHoods(initialNumber = 0, expansion1989 = 89, expansion2019 = 19) 
+  return initialNumber + expansion1989 + expansion2019;
+}
