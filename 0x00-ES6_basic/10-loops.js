06454e851b41d63f2afc80fa723aec0d84bc7631
theurikenneth <theurikenneth@gmail.com>
2021-11-23 09:00:19 +0000

    ES6 Basics

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,3 +1,4 @@
+// Rewrite appendToEachArrayValue to use ES6\'s for...of operator
 export default function appendToEachArrayValue(array, appendString) {
   const lst = [];
   for (const value of array) {
