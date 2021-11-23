296f668b57c5db513ddf953da9f60b67de5d9b5d
theurikenneth <theurikenneth@gmail.com>
2021-11-23 13:53:34 +0000

    ES6 Promises

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,11 @@
 // Return a promise using thus prototype function getResponseFromAPI()
 function getResponseFRomAPI() {
   return new Promise(function(resolve, reject) => {
+    // if true, resolve to "Stuff worked"    
     if (true) {
       resolve("Stuff worked!");
-    }
-    else {
+    } else {
+    // if false, reject with the message "it broke"
       reject(Error("It broke"));
     }
   });
--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -3,10 +3,12 @@
 export default function getFullResponseFromAPI(success) {
   const myPromise = new Promise((resolve, reject) => {
     if (success) {
+    // resolve the promise by passing an object with 2 attributes
       resolve(
         { status: 200, body: 'Success' },
       );
     } else {
+    // reject the promise with an error
       reject(new Error('The fake API is not working currently'));
     }
   });
