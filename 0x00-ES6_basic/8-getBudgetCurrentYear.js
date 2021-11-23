2229e10af85582d3d9640825eac1353f0ded7e8d
theurikenneth <theurikenneth@gmail.com>
2021-11-23 08:32:24 +0000

    ES6 Basics

---
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,16 @@
+// uses the ES6 computed property names on the budget object
+function getCurrentYear() {
+  const date = new Date();
+  return date.getFullYear();
+}
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const budget = {
+
+  [`income-${getCurrentYear()}`]: income,
+  [`gdp-${getCurrentYear()}`]: gdp,
+  [`capita-${getCurrentYear()}`]: capita,
+  };
+
+  return budget;
+}
