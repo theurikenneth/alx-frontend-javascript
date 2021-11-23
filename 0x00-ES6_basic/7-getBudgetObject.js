ed412048a44130aae4bed53b6e7b820d35e915c2
theurikenneth <theurikenneth@gmail.com>
2021-11-23 08:09:13 +0000

    ES6 Basics

---
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,10 @@
+//modify function\'s budget object to simply use the keyname instead
+export default function getBudgetObject(income, gdp, capital) {
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
+
+  return budget;
+}
