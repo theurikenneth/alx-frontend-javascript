36dec1e494301c4f733a88e2fa4fbddb9206d1a8
theurikenneth <theurikenneth@gmail.com>
2021-11-24 10:45:41 +0000

    ES6 Promises

---
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -0,0 +1,9 @@
+/* Write and export a function loadBalancer
+   It accepts two arguments: chinaDownload(Promise)
+   and USDownload(Promise) */
+export default function loadBalancer(chinaDownload, USDownload) {
+  /* uses .race()returns a Promise that is settled the same way 
+     (and takes the same value) as the first promise that settles
+     amongst the promises of the iterable passed as an argument */
+  return Promise.race([chinaDownload, USDownload]);
+}
