6063b782f52c806409ac9067df8cb6855659a9a6
theurikenneth <theurikenneth@gmail.com>
2021-11-24 08:50:59 +0000

    ES6 Promises

---
+++ 0x01-ES6_promise/4-user-promise.js
@@ -0,0 +1,11 @@
+// Using prototype to resolve promise to signup
+function signUpUser(firstName, lastName) {
+  return Promise.resolve(
+    {
+      firstName,
+      lastName.
+    },
+  );
+}
+
+export default signUpUser;
