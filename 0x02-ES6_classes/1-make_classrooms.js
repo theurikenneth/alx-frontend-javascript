94591936d458c14fe16cdda12086f91e10adb076
theurikenneth <theurikenneth@gmail.com>
2021-11-25 20:37:04 +0000

    ES6 classes

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,7 +1,7 @@
 /* Import ClassRoom class from 0-classroom.js
    Implement the function initializeRooms
    Returns an array with sizes 19, 20 and 34 */
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
   // implement a function initializeRooms
   export default function initializeRooms() {
     return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
