e20276ffdaeb137a1f189e072b1f383aaf2da0c7
theurikenneth <theurikenneth@gmail.com>
2021-11-25 05:05:02 +0000

    ES6 classes

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,11 @@
+/* Implement a class ClassRoom that accepts one attribute
+   maxStudentsSize */
+export default class ClassRoom {
+  /* constructor method helps create and initialize
+     an object created with a class */
+  constructor(maxStudentSize) {
+    /* https://stackoverflow.com/questions/4484424/is-the-underscore-prefix-for-property-and-method-names-merely-a-convention
+       this keyword refers to the object it belongs to */
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
