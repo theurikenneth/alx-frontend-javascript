8c61d17116f1908c9f062bfeeaf1e9ef318fc40c
theurikenneth <theurikenneth@gmail.com>
2021-11-25 05:46:35 +0000

    ES6 classes

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,59 @@
+/* Implement a clas HolbertonCourse
+   Constructor attributes are name, length and students
+   attributes are scored in underscore attribute */
+export default function HolbertonCourse() {
+  // initialize the constructor
+  constructor(name='', length = 0, students = []) {
+    // this is similar to self in python
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  // get returns name element from map object
+  get name() {
+    return this._name;
+  }
+
+  // set allows storage of unique values
+  set name(name_value) {
+    // checks whether name_value is a string
+    if (typeof name_value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = name_value;
+  }
+
+  // get returns length element
+  get length() {
+    return this._length;
+  }
+
+  // set allows storage of length value
+  set length(length_value) {
+    // checks whether length_value is a number
+    if (typeof length_value !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = length_value;
+  }
+
+  // get returns students element
+  get students() {
+    return this._students;
+  }
+
+  // set allows storage of student value
+  set students(current_students) {
+    if (typeof current_students === 'object') {
+      for (const student in current_students) {
+        if (typeof student !== 'string') {
+          throw new TypeError('Students must be an array');
+        }
+      }
+    } else {
+      throw new TypeError('Students must be an array of strings');
+    }
+    this._students = current_students;
+  }
+}
