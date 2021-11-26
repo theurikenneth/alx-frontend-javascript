e72ed56b547472e1a611b9e78d279b615ee2a22e
theurikenneth <theurikenneth@gmail.com>
2021-11-26 08:55:09 +0000

    ES6 classes

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -11,28 +11,6 @@ export default class Airport {
   }
 
   get [Symbol.toStringTag]() {
-    return `${this.code}`;
-  }
-
-  get name() {
-    return this._name;
-  }
-
-  set name(name_value) {
-    if (typeof name_value !== 'string') {
-      throw new TypeError('name must be a string');
-    }
-    this._name = name_value;
-  }
-
-  get code() {
-    return this._code;
-  }
-
-  set code(code_value) {
-    if (typeof code_value !== 'string') {
-      throw new TypeError('code must be a string');
-    }
-    this._code = code_value;
+    return this.code;
   }
 }
