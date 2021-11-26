e9a5158f3dbe1628f288b3cd4b424c33b43b4b48
theurikenneth <theurikenneth@gmail.com>
2021-11-26 08:16:34 +0000

    ES6 classes

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,17 @@
+/* Implement class Building with attribute
+   sqft(Number) and must have an underscore
+   attribute */
+export default class Building {
+  constructor(sqft = 0) {
+    if (this.constructor !== Building
+	&& typeof this.evacuationWarningMessage !== 'function') {
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    }
+
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
