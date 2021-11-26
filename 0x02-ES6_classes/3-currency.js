8e08bc21a28c9ac5725ccb8188cfeccc2dd118da
theurikenneth <theurikenneth@gmail.com>
2021-11-26 11:38:32 +0000

    ES6 classes

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -16,9 +16,6 @@ export default class Currency {
 
   // setter for code
   set code(code_value) {
-    if (typeof code_value !== 'string') {
-      throw TypeError('Code must be a string');
-    }
     this._code = code_value;
   }
 
@@ -28,9 +25,6 @@ export default class Currency {
   }
 
   set name(name_value) {
-    if (typeof name_value !== 'string' {
-      throw TypeError('Name must be a string');
-    }
     this._name = name_value;
   }
 
