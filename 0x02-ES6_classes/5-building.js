bde8a165ba230b3c144fb0d9cf9b260ca33ce54d
theurikenneth <theurikenneth@gmail.com>
2021-11-26 09:40:08 +0000

    ES6 classes

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -3,9 +3,13 @@
    attribute */
 export default class Building {
   constructor(sqft) {
-    if (this.constructor !== Building
-	&& typeof this.evacuationWarningMessage !== 'function') {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
+    if (
+      this.constructor !== Building
+      && typeof this.evacuationWarningMessage !== 'function'
+    ) {
+      throw new Error(
+        'Class extending Building must override evacuationWarningMessage',
+      );
     }
 
     this._sqft = sqft;
