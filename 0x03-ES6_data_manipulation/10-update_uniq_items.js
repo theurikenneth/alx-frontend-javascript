f9023ef261699e1f8870140519e79c1fa2b6b8e1
theurikenneth <theurikenneth@gmail.com>
2021-11-29 20:52:12 +0000

    ES6 data manipulation

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,8 @@
+/* Create a function named updateUniqueItems
+   accepts a map as an argument */
+export default function updateUniqueItems(map) {
+  // if not an instance of map throws an error
+  if (!(map instance Map)) throw new Error('Cannot process');
+  for (const x of map) if (x[1] === 1) map.set(i[0], 100);
+  return map;
+}
